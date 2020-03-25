62a8182c036a79d7b9ea981192d5a5594b5469f5
shambles1812 <cjvillafuerte02@gmail.com>
2020-03-25 19:57:12 +0800

    With new outside file for Shopify Create Order API

--- customPackage/__init__.py
+++ customPackage/__init__.py
@@ -2,12 +2,20 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+import shopify as shopify
+
+shopify_api_key = 'fea943bb49a20eb4fbb51563dd946c1f'
+shopify_api_password = '94dcb3f846b271fe237b5052d138054b'
+shopify_api_version = '7.0.1'
 
 application = app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+shop_url = "https://%s:%s@SHOP_NAME.myshopify.com/admin/api/%s" % (shopify_api_key,shopify_api_password , shopify_api_version)
+shopify.ShopifyResource.set_site(shop_url)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+
 
 from customPackage import routes
---
+++ shopify_Create_DraftOrder.py
@@ -0,0 +1,26 @@
+import shopify as shopify
+
+# API Credentials
+API_KEY = 'fea943bb49a20eb4fbb51563dd946c1f'
+PASSWORD = '94dcb3f846b271fe237b5052d138054b'
+SHARED_SECRET = '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'
+API_VERSION = '2020-01'
+SHOP_NAME = 'developer-test-software'
+
+# Shop Link
+shop_url = "https://%s:%s@%s.myshopify.com/admin/api/%s" % (API_KEY,PASSWORD,SHOP_NAME, API_VERSION)
+shopify.ShopifyResource.set_site(shop_url)
+
+
+# Create a Draft Order
+order =shopify.DraftOrder()
+order.line_items = [{"title": "Custom Tee",
+        "price": "20.00",
+        "quantity": 2}]
+order.save()
+success = order.save()
+
+# Checks whether the order is saved
+print(success)
+if order.errors:
+   print (order.errors.full_messages())
